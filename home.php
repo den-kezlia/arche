a26512f9f08ce34533554324694ddab3ec0b333a
Den Kezlia <xian.den@gmail.com>
2016-03-31 23:58:07 +0300

    use html file instead php files

--- home.php
+++
@@ -1,8 +0,0 @@
-<article class="home active">
-    <h1>Веб Студиа "Arche"</h1>
-    <h2>
-        It is a long established fact that a reader will <br/>
-        be distracted by the readable content.
-    </h2>
-    <a href="/blog" id="blog-link">Блог</a>
-</article>
